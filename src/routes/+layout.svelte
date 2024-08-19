7abd30dc2d74d0c253a61b52e3b8b0a26407dacd
iamajraj <akmalraj07@gmail.com>
2024-08-19 23:20:48 +0600

    layout navigation link updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,9 +8,9 @@
 		<div class="container mx-auto flex items-center justify-between px-6">
 			<a href="/" class="text-3xl font-bold text-white">Live Text Share</a>
 			<ul class="flex space-x-6 text-lg text-white">
-				<li><a href="#features" class="hover:underline">Features</a></li>
-				<li><a href="#get-started" class="hover:underline">Get Started</a></li>
-				<li><a href="#contact" class="hover:underline">Feedback</a></li>
+				<li><a href="/#features" class="hover:underline">Features</a></li>
+				<li><a href="/#get-started" class="hover:underline">Get Started</a></li>
+				<li><a href="/#contact" class="hover:underline">Feedback</a></li>
 			</ul>
 		</div>
 	</nav>
